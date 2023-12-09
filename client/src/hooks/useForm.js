aab51ea0d1bef01cfe09a71b468dfcdc2acb6495
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-12-09 16:47:33 +0200

    added input trim in useForm hook
    added input trim in useForm hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -14,10 +14,22 @@ export default function useForm(submitHandler, initialValues) {
         }));
     };
 
+
     const onSubmit = (e) => {
         e.preventDefault();
 
-        submitHandler(values);
+        const trimedValues =  Object.entries(values).reduce((acc, inputField) => {                                         // Using Object.entries to process each key-value pair in the input object
+            const [inputFieldName, inputFieldValue] = inputField;
+
+            if (inputFieldValue === '') {                                                               // If the value of the input field is an empty string, skip it in the result
+                return acc;
+            }
+
+            return { ...acc, [inputFieldName]: typeof inputFieldValue === 'string' ? inputFieldValue.trim() : inputFieldValue };
+        }, {});
+
+
+        submitHandler(trimedValues);
     };
 
     return {
